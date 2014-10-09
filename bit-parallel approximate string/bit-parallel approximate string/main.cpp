f7fdb3724b522fdd3d8a070e9a9135c56c114e3e
GangLiao <greenhat1016@gmail.com>
2014-10-09 11:02:22 +0300

    start a project
    bit-parallel approximate

---
+++ bit-parallel approximate string/bit-parallel approximate string/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  bit-parallel approximate string
+//
+//  Created by gangliao on 10/9/14.
+//  Copyright (c) 2014 gangliao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
